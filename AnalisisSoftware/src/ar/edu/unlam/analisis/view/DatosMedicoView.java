1b21a74a0921b8446c516ceb281273dee140c386
Pablo Becerra <pablo_becerra94@yahoo.com.ar>
2017-06-08 10:03:40 -0300

    Comentarios en dos vistas

--- AnalisisSoftware/src/ar/edu/unlam/analisis/view/DatosMedicoView.java
+++ AnalisisSoftware/src/ar/edu/unlam/analisis/view/DatosMedicoView.java
@@ -20,102 +20,102 @@ public class DatosMedicoView extends JFrame{
 	private JTextField textNombre;
 	private JTextField textEspecializacion;
 	public DatosMedicoView() {
-		getContentPane().setLayout(null);
-		setSize(new Dimension(500, 500));
-		setLocationRelativeTo(null);
-		JLabel lblNuevoMedico = new JLabel("Nuevo Medico");
-		lblNuevoMedico.setBounds(160, 6, 100, 16);
-		getContentPane().add(lblNuevoMedico);
+		getContentPane().setLayout(null); //setea un layout absoluto
+		setSize(new Dimension(500, 500)); //setea la dimension de la ventana
+		setLocationRelativeTo(null); //setea que no este relativa a nada la pantalla
+		JLabel lblNuevoMedico = new JLabel("Nuevo Medico"); //crea un label
+		lblNuevoMedico.setBounds(160, 6, 100, 16); //con esas medidas
+		getContentPane().add(lblNuevoMedico); //agregadandolo a la ventana
 		
-		textCodigo = new JTextField();
-		textCodigo.setBounds(140, 34, 130, 26);
-		getContentPane().add(textCodigo);
-		textCodigo.setColumns(10);
+		textCodigo = new JTextField(); //crea el campo para ingresar codigo
+		textCodigo.setBounds(140, 34, 130, 26); //setea las dimensiones
+		getContentPane().add(textCodigo); //lo agrega a la ventana
+		textCodigo.setColumns(10); //con 10 columnas 
 		
-		textNombre = new JTextField();
-		textNombre.setBounds(140, 65, 130, 26);
-		getContentPane().add(textNombre);
-		textNombre.setColumns(10);
+		textNombre = new JTextField(); //crea el campo para ingresar nombre
+		textNombre.setBounds(140, 65, 130, 26);  //setea las dimensiones
+		getContentPane().add(textNombre); //lo agrega a la ventana
+		textNombre.setColumns(10); //con 10 columnas 
 		
-		textEspecializacion = new JTextField();
-		textEspecializacion.setBounds(140, 103, 130, 26);
-		getContentPane().add(textEspecializacion);
-		textEspecializacion.setColumns(10);
+		textEspecializacion = new JTextField(); //crea el campo para ingresar especializacion
+		textEspecializacion.setBounds(140, 103, 130, 26); //setea las dimensiones
+		getContentPane().add(textEspecializacion); //lo agrega a la ventana
+		textEspecializacion.setColumns(10);  //con 10 columnas 
 		
-		JLabel lblCodigo = new JLabel("Codigo:");
-		lblCodigo.setBounds(37, 39, 61, 16);
-		getContentPane().add(lblCodigo);
+		JLabel lblCodigo = new JLabel("Codigo:"); //crea un label
+		lblCodigo.setBounds(37, 39, 61, 16); //setea las dimensiones
+		getContentPane().add(lblCodigo);  //lo agrega a la ventana
+		 
+		JLabel lblNombre = new JLabel("Nombre:"); //crea un label
+		lblNombre.setBounds(37, 70, 61, 16); //setea las dimensiones
+		getContentPane().add(lblNombre); //lo agrega a la ventana
 		
-		JLabel lblNombre = new JLabel("Nombre:");
-		lblNombre.setBounds(37, 70, 61, 16);
-		getContentPane().add(lblNombre);
+		JLabel lblEspecializacion = new JLabel("Especializacion:"); //crea un label
+		lblEspecializacion.setBounds(37, 108, 102, 16); //setea las dimensiones
+		getContentPane().add(lblEspecializacion); //lo agrega a la ventana
 		
-		JLabel lblEspecializacion = new JLabel("Especializacion:");
-		lblEspecializacion.setBounds(37, 108, 102, 16);
-		getContentPane().add(lblEspecializacion);
-		
-		JButton btnLimpiar = new JButton("Limpiar");
-		btnLimpiar.setToolTipText("Presione este botón para vaciar los campos");
-		btnLimpiar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				limpiar();
+		JButton btnLimpiar = new JButton("Limpiar"); //crea el boton
+		btnLimpiar.setToolTipText("Presione este botón para vaciar los campos"); //agrega el tooltip text
+		btnLimpiar.addActionListener(new ActionListener() { //agrega un listener para escuchar el clicks
+			public void actionPerformed(ActionEvent e) { //metodo para escuchar clicks
+				limpiar(); //limpia la pantalla
 			}
 		});
-		btnLimpiar.setBounds(70, 154, 117, 29);
-		getContentPane().add(btnLimpiar);
+		btnLimpiar.setBounds(70, 154, 117, 29); //setea las dimensiones
+		getContentPane().add(btnLimpiar); //lo agrega a la ventana
 		
-		JButton btnRegistrar = new JButton("Registrar");
-		btnRegistrar.setToolTipText("Presione este botón para guardar un nuevo médico con los datos ingresados");
-		btnRegistrar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				StringBuilder errors = new StringBuilder("");
-				if(textCodigo.getText().isEmpty()){
-					errors.append("*El codigo es necesario.").append("\n");
+		JButton btnRegistrar = new JButton("Registrar"); //crea el boton
+		btnRegistrar.setToolTipText("Presione este botón para guardar un nuevo médico con los datos ingresados"); //agrega el tooltip text
+		btnRegistrar.addActionListener(new ActionListener() { //agrega un listener para escuchar el clicks
+			public void actionPerformed(ActionEvent e) {  //metodo para escuchar clicks
+				StringBuilder errors = new StringBuilder(""); //crea un builder de string
+				if(textCodigo.getText().isEmpty()){ //si el campo codigo esta vacio
+					errors.append("*El codigo es necesario.").append("\n"); //le agrega al string builder el error
 				}
-				if(textEspecializacion.getText().isEmpty()){
-					errors.append("*La especializacion es necesaria.").append("\n");
+				if(textEspecializacion.getText().isEmpty()){ //si el campo especializacion esta vacio
+					errors.append("*La especializacion es necesaria.").append("\n");  //le agrega al string builder el error
 				}
-				if(textNombre.getText().isEmpty()){
-					errors.append("*El nombre es necesario.").append("\n");
+				if(textNombre.getText().isEmpty()){  //si el campo nombre esta vacio
+					errors.append("*El nombre es necesario.").append("\n"); //le agrega al string builder el error
 				}
 				
-				if(errors.toString().isEmpty()){
+				if(errors.toString().isEmpty()){ //si no hay errores
 					try {
-						controlpac.nuevoMedico(textCodigo.getText(), textNombre.getText(), textEspecializacion.getText(),true);
-						HandleResponseUtil.showMessageSuccess(HandleResponseUtil.COMMON_SUCCESS);
-						limpiar();
+						controlpac.nuevoMedico(textCodigo.getText(), textNombre.getText(), textEspecializacion.getText(),true); //crea el medico
+						HandleResponseUtil.showMessageSuccess(HandleResponseUtil.COMMON_SUCCESS); //muestra el mensaje de exito
+						limpiar(); //limpia la pantalla
 					} catch (Exception e1) {
-						HandleResponseUtil.showMessageError(HandleResponseUtil.COMMON_ERROR);
+						HandleResponseUtil.showMessageError(HandleResponseUtil.COMMON_ERROR); //muesta el mensaje de error
 					}
 				}else{
-					HandleResponseUtil.showMessageError(errors.toString());
+					HandleResponseUtil.showMessageError(errors.toString()); //muestra los mensajes de error
 				}
 			}
 		});
-		btnRegistrar.setBounds(217, 154, 117, 29);
-		getContentPane().add(btnRegistrar);
+		btnRegistrar.setBounds(217, 154, 117, 29); //setea las dimensiones
+		getContentPane().add(btnRegistrar); //lo agrega a la ventana
 		
-		JButton btnVolver = new JButton("Volver");
-		btnVolver.setToolTipText("Presione este botón para volver al menú anterior");
-		btnVolver.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				IngresoDatosView idw = new IngresoDatosView();
-				idw.setVisible(true);
-				dispose();
+		JButton btnVolver = new JButton("Volver"); //crea el boton volver
+		btnVolver.setToolTipText("Presione este botón para volver al menú anterior"); //crea el tooltip text
+		btnVolver.addActionListener(new ActionListener() { //crea listener para escuchar clicks
+			public void actionPerformed(ActionEvent e) { //metodo para escuchar clicks
+				IngresoDatosView idw = new IngresoDatosView(); //crea la pantalla de ingreso de datos
+				idw.setVisible(true); //muestra la pantall de ingreso de datos
+				dispose(); //cierra la pantalla de ingreso de datos del medico
 			}
 		});
-		btnVolver.setBounds(327, 243, 117, 29);
-		getContentPane().add(btnVolver);
-		JLabel label = new JLabel("Usuario: "+UserProvider.getUsuarioLogueado());
+		btnVolver.setBounds(327, 243, 117, 29); //setea las dimensiones
+		getContentPane().add(btnVolver); //agrega el boton volver
+		JLabel label = new JLabel("Usuario: "+UserProvider.getUsuarioLogueado()); //crea uyn label
 		label.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
-		label.setBounds(6, 17, 90 + label.getText().length(), 16);
-		getContentPane().add(label);
+		label.setBounds(6, 17, 90 + label.getText().length(), 16); //setea las dimensiones
+		getContentPane().add(label); //lo agrega a la ventana
 	}
 	
-	public void limpiar(){
-		textCodigo.setText("");
-		textEspecializacion.setText("");
-		textNombre.setText("");
+	public void limpiar(){ //metodo para limpiar la pantalla 
+		textCodigo.setText(""); //setea 
+		textEspecializacion.setText(""); //todos los campos
+		textNombre.setText(""); //con una cadena vacia
 	}
 
 }
--- AnalisisSoftware/src/ar/edu/unlam/analisis/view/DatosPacienteView.java
+++ AnalisisSoftware/src/ar/edu/unlam/analisis/view/DatosPacienteView.java
@@ -18,89 +18,91 @@ import ar.edu.unlam.analisis.util.HandleResponseUtil;
 public class DatosPacienteView extends JFrame {
 	private JTextField textFieldCodigo;
 	private JTextField textFieldNombre;
+	
 	public DatosPacienteView() {
-		getContentPane().setLayout(null);
-		setSize(new Dimension(500, 500));
-		setLocationRelativeTo(null);
-		JButton btnRegistrar = new JButton("Registrar");
-		btnRegistrar.setToolTipText("Presione este botón para guardar un nuevo paciente con los datos ingresados");
-		btnRegistrar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				StringBuilder errors = new StringBuilder("");
-				if(textFieldCodigo.getText().isEmpty()){
-					errors.append("*El codigo es necesario").append("\n");
+		getContentPane().setLayout(null); //setea un layout absoluto
+		setSize(new Dimension(500, 500));  //setea la dimension de la ventana
+		setLocationRelativeTo(null); //setea que no este relativa a nada la pantalla
+		JButton btnRegistrar = new JButton("Registrar"); //crea un boton para registrar
+		btnRegistrar.setToolTipText("Presione este botón para guardar un nuevo paciente con los datos ingresados"); //Crea el tooltip
+		btnRegistrar.addActionListener(new ActionListener() { //crea el listener para escuchar clicks
+			public void actionPerformed(ActionEvent e) { //metodo para escuchar clicks
+				StringBuilder errors = new StringBuilder(""); //builder de string para errores
+				if(textFieldCodigo.getText().isEmpty()){ //si el codigo esta vacio
+					errors.append("*El codigo es necesario").append("\n"); //agrega el error
 				}
-				if(textFieldNombre.getText().isEmpty()){
-					errors.append("*El nombre es necesario").append("\n");
+				if(textFieldNombre.getText().isEmpty()){ //si el nombre esta vacio
+					errors.append("*El nombre es necesario").append("\n"); //agrega el error
 				}
 				
-				if(errors.toString().isEmpty()){
+				if(errors.toString().isEmpty()){ //si la lista de errores esta vacia
 					try {
-						controlpac.nuevoPaciente(textFieldCodigo.getText(), textFieldNombre.getText(),true);
-						limpiar();
-						HandleResponseUtil.showMessageSuccess(HandleResponseUtil.COMMON_SUCCESS);
+						controlpac.nuevoPaciente(textFieldCodigo.getText(), textFieldNombre.getText(),true); //crea el paciente
+						limpiar(); //limpia la pantalla
+						HandleResponseUtil.showMessageSuccess(HandleResponseUtil.COMMON_SUCCESS); //muestra el mensaje de exito
 					} catch (Exception e1) {
-						HandleResponseUtil.showMessageError(HandleResponseUtil.COMMON_ERROR);
+						HandleResponseUtil.showMessageError(HandleResponseUtil.COMMON_ERROR); //muestra el mensaje de error
 					}
 				}else{
-					HandleResponseUtil.showMessageError(errors.toString());
+					HandleResponseUtil.showMessageError(errors.toString()); //muestra el mensaje de error
 				}
 			}
 		});
-		btnRegistrar.setBounds(264, 132, 117, 29);
-		getContentPane().add(btnRegistrar);
+		btnRegistrar.setBounds(264, 132, 117, 29); //agrega las dimensiones
+		getContentPane().add(btnRegistrar); //agrega el boton a la ventana
 		
-		JButton btnLimpiar = new JButton("Limpiar");
-		btnLimpiar.setToolTipText("Presione este botón para vaciar los campos");
-		btnLimpiar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				limpiar();
+		JButton btnLimpiar = new JButton("Limpiar"); //crea un boton para limpiar
+		btnLimpiar.setToolTipText("Presione este botón para vaciar los campos"); //setea el tooltip
+		btnLimpiar.addActionListener(new ActionListener() { //agrega el listener para escuchar clicks
+			public void actionPerformed(ActionEvent e) { //metodo para escuchar clicks
+				limpiar(); //limpia la pantalla 
 			}
 		});
-		btnLimpiar.setBounds(86, 132, 117, 29);
-		getContentPane().add(btnLimpiar);
+		btnLimpiar.setBounds(86, 132, 117, 29); //setea las dimensiones
+		getContentPane().add(btnLimpiar); //limpia la pantalla
 		
-		JButton btnVolver = new JButton("Volver");
-		btnVolver.setToolTipText("Presione este botón para volver al menú anterior");
-		btnVolver.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				IngresoDatosView idw = new IngresoDatosView();
-				idw.setVisible(true);
-				dispose();
+		JButton btnVolver = new JButton("Volver"); //crea el boton para volver
+		btnVolver.setToolTipText("Presione este botón para volver al menú anterior"); //setea el tooltip
+		btnVolver.addActionListener(new ActionListener() { //agrega el listener para escuchar clicks
+			public void actionPerformed(ActionEvent e) { //metodo para eschuchar clicks
+				IngresoDatosView idw = new IngresoDatosView(); //crea la ventana de ingreso de datos
+				idw.setVisible(true); //muestra la pantalla
+				dispose(); //cierra la pantalla actual
 			}
 		});
-		btnVolver.setBounds(327, 243, 117, 29);
-		getContentPane().add(btnVolver);
+		btnVolver.setBounds(327, 243, 117, 29); //setea las dimensiones 
+		getContentPane().add(btnVolver); //agrega el boton a la ventana
 		
-		textFieldCodigo = new JTextField();
-		textFieldCodigo.setBounds(159, 45, 178, 26);
-		getContentPane().add(textFieldCodigo);
-		textFieldCodigo.setColumns(10);
+		textFieldCodigo = new JTextField(); //crea el campo para ingresar el codigo
+		textFieldCodigo.setBounds(159, 45, 178, 26); //Setea las dimensiones
+		getContentPane().add(textFieldCodigo); //agrega el campo a la ventana
+		textFieldCodigo.setColumns(10); //setea las columnas 
 		
-		textFieldNombre = new JTextField();
-		textFieldNombre.setBounds(159, 79, 178, 26);
-		getContentPane().add(textFieldNombre);
-		textFieldNombre.setColumns(10);
+		textFieldNombre = new JTextField(); //crea el campo para ingresar nombre
+		textFieldNombre.setBounds(159, 79, 178, 26); //setea las dimensiones
+		getContentPane().add(textFieldNombre); //agrega el campo a la ventana
+		textFieldNombre.setColumns(10); //setea las columnas
 		
-		JLabel lblCodigo = new JLabel("Codigo:");
-		lblCodigo.setBounds(86, 50, 61, 16);
-		getContentPane().add(lblCodigo);
+		JLabel lblCodigo = new JLabel("Codigo:"); //crea el label para codigo
+		lblCodigo.setBounds(86, 50, 61, 16); //setea las dimensiones
+		getContentPane().add(lblCodigo); //agrega el label a la ventana
 		
-		JLabel lblNombre = new JLabel("Nombre:");
-		lblNombre.setBounds(86, 84, 61, 16);
-		getContentPane().add(lblNombre);
+		JLabel lblNombre = new JLabel("Nombre:");//crea el label 
+		lblNombre.setBounds(86, 84, 61, 16); //setea las dimensiones
+		getContentPane().add(lblNombre); //agrega el label a la ventana
 		
-		JLabel lblNuevoPaciente = new JLabel("Nuevo Paciente");
-		lblNuevoPaciente.setBounds(183, 6, 103, 16);
-		getContentPane().add(lblNuevoPaciente);
-		JLabel label = new JLabel("Usuario: "+UserProvider.getUsuarioLogueado());
-		label.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
-		label.setBounds(6, 17, 90 + label.getText().length(), 16);
-		getContentPane().add(label);
+		JLabel lblNuevoPaciente = new JLabel("Nuevo Paciente"); //crea el label 
+		lblNuevoPaciente.setBounds(183, 6, 103, 16); //setea las dimensiones
+		getContentPane().add(lblNuevoPaciente); //agrega el label a la ventana
+		
+		JLabel label = new JLabel("Usuario: "+UserProvider.getUsuarioLogueado()); //crea el label 
+		label.setFont(new Font("Lucida Grande", Font.PLAIN, 10)); //setea la fuente
+		label.setBounds(6, 17, 90 + label.getText().length(), 16);//setea las dimensiones
+		getContentPane().add(label); //agrega el label a la ventana
 	}
 	
-	public void limpiar(){
-		textFieldCodigo.setText("");
-		textFieldNombre.setText("");
+	public void limpiar(){ //metodo para limpiar la pantalla
+		textFieldCodigo.setText(""); //setea los campos
+		textFieldNombre.setText("");//con una cadena vacia
 	}
 }
